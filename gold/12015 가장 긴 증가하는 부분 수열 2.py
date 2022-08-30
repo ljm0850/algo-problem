8529f7ff637c5bd09c400bdbb59a54b367647fbd
ljm0850 <dlwoals0850@gmail.com>
2022-08-30 21:52:53 +0900

    0830

--- gold/12015 가장 긴 증가하는 부분 수열 2.py
+++ gold/12015 가장 긴 증가하는 부분 수열 2.py
@@ -1,22 +1,23 @@
-def solve(n:int,nums:list)->tuple:
-    dp = [[1,[nums[_]]] for _ in range(n)]
+import sys
+def solve(arr:list)->int:
+    dp = [0]
+    cnt = 0
+    for num in arr:
+        if dp[-1] < num:
+            dp.append(num)
+            cnt += 1
+        else:
+            start, end = 0, cnt
+            while start < end:
+                mid = (start + end) // 2
+                if dp[mid] < num:
+                    start = mid + 1
+                else:
+                    end = mid
+            dp[end] = num
+    return cnt
 
-    for i in range(1,n):
-        for j in range(i):
-            if nums[i] > nums[j]:
-                if dp[i][0] < dp[j][0]+1:
-                    dp[i][0] = dp[j][0] +1
-                    dp[i][1] = dp[j][1] + [nums[i]]
-    max_cnt = dp[0][0]
-    max_sequence = dp[0][1]
-    for i in range(1,n):
-        if dp[i][0] > max_cnt:
-            max_cnt = dp[i][0]
-            max_sequence = dp[i][1]
-    return (max_cnt,max_sequence)
-
-N = int(input())
-target = list(map(int,input().split()))
-ans = solve(N,target)
-print(ans[0])
-print(*ans[1])
+N = int(sys.stdin.readline())
+total_sequence = list(map(int,sys.stdin.readline().split()))
+ans = solve(total_sequence)
+print(ans)
